84f3f04da7cec577a4c3cc45e8b54b0dd2cc6164
Mileanie <mileanie17@gmail.com>
2021-06-12 09:45:26 +0530

    done

--- Module1/Lec3_Async/nFiles/asyncSerial.js
+++ Module1/Lec3_Async/nFiles/asyncSerial.js
@@ -1,2 +1,13 @@
 let fs = require("fs");
 let files = ["../f1.txt" , "../f2.txt" , "../f3.txt"];
+let i=0;
+function serial(){
+    fs.readFile(files[i], function (err, data) {
+        i=i+1;
+        console.log(data + "");
+        if(i<files.length){
+            serial();
+        }
+    })
+}
+serial()
